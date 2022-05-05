f7057a76a6213a9547cbbe02c6048e9383a7e88a
Renan Batista <renfbatista@gmail.com>
2022-05-05 00:20:51 -0300

    Add vim configs

--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
+# Change vim directory
+export VIMINIT="source ~/dotfiles/.vimrc"
+ln -sf ~/dotfiles/.vimrc ~/.vimrc 
+
+# Vim plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # NVIM
 sudo add-apt-repository ppa:neovim-ppa/unstable
