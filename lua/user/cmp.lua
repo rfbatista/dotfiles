39d1b1fecfd965a31d856041ab8742b6b7b4f9b7
Renan Batista <renfbatista@gmail.com>
2022-05-07 21:06:00 -0300

    some updates

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -119,8 +119,8 @@ cmp.setup {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
   },
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  window = {
+    documentation = cmp.config.window.bordered(), 
   },
   experimental = {
     ghost_text = false,
