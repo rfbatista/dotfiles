0402e2d092b4c28d195fa9de27efe4c038cac304
Renan Batista <renfbatista@gmail.com>
2022-05-09 12:59:34 -0300

    nvim sneak plugin

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -93,6 +93,7 @@ nvim_tree.setup {
         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
         { key = "h", cb = tree_cb "close_node" },
         { key = "v", cb = tree_cb "vsplit" },
+        { key = "<C-n>", cb = tree_cb "create" },
       },
     },
     number = false,
