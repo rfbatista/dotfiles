18e39aeea233166e5d7526c1d85a9b288aae1ae8
Renan Batista <renfbatista@gmail.com>
2022-05-22 11:21:07 -0300

    i3 and polybar configs

---
+++ install-fonts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Dirs
+FDIR="$HOME/.local/share/fonts"
+
+# Install Fonts
+install_fonts() {
+	echo -e "\n[*] Installing fonts..."
+	if [[ -d "$FDIR" ]]; then
+		cp -rf ~/dotfiles/fonts/* "$FDIR"
+	else
+		mkdir -p "$FDIR"
+		cp -rf ~/dotfiles/fonts/* "$FDIR"
+	fi
+    echo -e "\nFonts installed."
+}
+
+install_fonts
