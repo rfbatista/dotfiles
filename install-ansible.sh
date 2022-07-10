e047135455b472195543f05133446bc333ee3ba7
Renan Batista <renfbatista@gmail.com>
2022-07-10 12:28:25 -0300

    install ansible

---
+++ install-ansible.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+## Install ansible
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update -y
+sudo apt-get install -y curl git software-properties-common ansible
