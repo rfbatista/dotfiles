05206d3ad2e362aaab24fce7be37d1cdb8df9db7
Renan Batista <renfbatista@gmail.com>
2022-10-08 22:34:43 -0300

    change lsp config for python

--- nvim/lua/user/languages/lsp/installer.lua
+++ nvim/lua/user/languages/lsp/installer.lua
@@ -19,30 +19,6 @@ local servers = {
 	"svelte",
 }
 
--- setup pipenv 
-local configs = require('lspconfig/configs')
-local util = require('lspconfig/util')
-
-local path = util.path
-
-local function get_python_path(workspace)
-  -- Use activated virtualenv.
-  if vim.env.VIRTUAL_ENV then
-    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
-  end
-
-  -- Find and use virtualenv in workspace directory.
-  for _, pattern in ipairs({'*', '.*'}) do
-    local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
-    if match ~= '' then
-      return path.join(path.dirname(match), 'bin', 'python')
-    end
-  end
-
-  -- Fallback to system Python.
-  return exepath('python3') or exepath('python') or 'python'
-end
-
 lsp_installer.setup({
 	ensure_installed = servers,
 	ui = {
