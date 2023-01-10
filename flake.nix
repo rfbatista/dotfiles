2a00e0892db9b03b5f416a094d5880728ebcd6e9
rfbatista <renfbatista@gmail.com>
2023-01-10 08:35:38 -0300

    flakes config

--- flake.nix
+++ flake.nix
@@ -1,31 +1,20 @@
 {
-  description = "NixOS configuration with two or more channels";
-
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-21.11"; 
-    nixpkgs-unstable.url = "nixpkgs/nixos-unstable"; 
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    neovim-nightly-overlay.inputs.neovim-flake.url = "github:neovim/neovim/v0.7.0?dir=contrib";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable }:
-    let
+  outputs = { self, nixpkgs, neovim-nightly-overlay, ... }: {
+    # replace 'joes-desktop' with your hostname here.
+    nixosConfigurations.rfbatista = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      overlay-unstable = final: prev: {
-        unstable = nixpkgs-unstable.legacyPackages.${prev.system};
-        # use this variant if unfree packages are needed:
-        # unstable = import nixpkgs-unstable {
-        #   inherit system;
-        #   config.allowUnfree = true;
-        # };
-
-      };
-    in {
-      nixosConfigurations.rfbatista = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          # Overlays-module makes "pkgs.unstable" available in configuration.nix
-          ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
-          ./configuration.nix
-        ];
-      };
+      modules = [ ./configuration.nix 
+        {
+          nixpkgs.overlays = [
+            (neovim-nightly-overlay.overlay)
+          ];
+        }
+      ];
     };
+  };
 }
