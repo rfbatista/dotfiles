69c80d98084f3c5bad4226e9d292c9bec1474d7c
rfbatista <renfbatista@gmail.com>
2023-08-02 10:04:57 -0300

    polybar em n monitores

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,6 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
 # Launch polybar
-polybar -c $HOME/dotfiles/polybar/config.ini -r i3 &
+    MONITOR=$m polybar -c $HOME/dotfiles/polybar/config.ini -r i3 &
+  done
+else
+# Launch polybar
+  polybar -c $HOME/dotfiles/polybar/config.ini -r i3 &
+fi
+
 
