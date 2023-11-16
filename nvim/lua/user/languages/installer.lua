4b2c4ac2a3afdb632af925363789d6e4d34d8c92
Renan Batista <renfbatista@gmail.com>
2023-11-16 18:13:17 -0300

    html e sql types

--- nvim/lua/user/languages/installer.lua
+++ nvim/lua/user/languages/installer.lua
@@ -1,53 +1,59 @@
 local configs = require("lspconfig.configs")
+
 configs.templ = {
-  default_config = {
-    cmd = { "templ", "lsp", "-http=localhost:7474", "-log=/Users/adrian/templ.log" },
-    filetypes = { "templ" },
-    settings = {},
-  },
+	default_config = {
+		--[[ cmd = { "templ", "lsp", "-http=localhost:7474", "-log=/Users/adrian/templ.log" }, ]]
+		filetypes = { "templ" },
+		settings = {},
+	},
 }
 
 local servers = {
-  "pyright",
-  "jsonls",
-  "lua_ls",
-  "gopls",
-  "clangd",
-  "tsserver",
-  "kotlin_language_server",
-  "terraformls",
-  "dockerls",
-  "sqls",
-  "templ",
+	"pyright",
+	"jsonls",
+	"lua_ls",
+	"gopls",
+	"clangd",
+	"tsserver",
+	"kotlin_language_server",
+	"terraformls",
+	"dockerls",
+	"sqls",
+	"templ",
 }
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
-  return
+	return
 end
 
 require("mason").setup({
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗",
-    },
-  },
-  ensure_installed = servers,
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
+	ensure_installed = servers,
 })
 require("mason-lspconfig").setup()
 require("mason-nvim-dap").setup({
-  ensure_installed = { "python", "delve", "cppdbg", "node2" },
-  automatic_installation = true,
+	ensure_installed = { "python", "delve", "cppdbg", "node2" },
+	automatic_installation = true,
 })
-
+vim.cmd([[
+  autocmd BufRead,BufNewFile *.html setfiletype html
+]])
+vim.cmd([[
+  autocmd BufRead,BufNewFile *.sql setfiletype sql
+]])
 lspconfig.sumneko_lua.setup(require("user.languages.configs.sumneko"))
 lspconfig.tsserver.setup(require("user.languages.configs.tsserver"))
 lspconfig.gopls.setup(require("user.languages.configs.gopls"))
 lspconfig.clangd.setup(require("user.languages.configs.cpp"))
 lspconfig.html.setup(require("user.languages.configs.html"))
 lspconfig.pyright.setup(require("user.languages.configs.python"))
-lspconfig.sqls.setup(require("user.languages.configs.sql"))
+--[[ lspconfig.sqls.setup(require("user.languages.configs.sql")) ]]
 lspconfig.templ.setup(require("user.languages.configs.templ"))
 lspconfig.tailwindcss.setup(require("user.languages.configs.tailwind"))
--- nvim/lua/user/languages/prettier.lua
+++ nvim/lua/user/languages/prettier.lua
@@ -1,1 +1,1 @@
-vim.g["prettier#autoformat_config_present"] = 1
+--[[ vim.g["prettier#autoformat_config_present"] = 1 ]]
