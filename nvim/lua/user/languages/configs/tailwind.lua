44166197e0e0ed6853e501852afbb38464f1ee53
Renan Batista <renfbatista@gmail.com>
2023-11-16 18:12:58 -0300

    new lsp

--- nvim/lua/user/languages/configs/tailwind.lua
+++ nvim/lua/user/languages/configs/tailwind.lua
@@ -2,20 +2,13 @@ local keymap = require("user.languages.lsp.keymap")
 
 local M = {}
 
-M.on_attach = function(client, bufnr)
-  keymap.on_attach(client, bufnr, { allowed_clients = { "gopls" }, format_on_save = true })
-end
+M.on_attach = keymap.on_attach
 
 M.filetypes = {
-  "templ",
-  "tsx",
-  "jsx",
-}
-
-M.init_options = {
-  userLanguages = {
-    templ = "html",
-  },
+	"templ",
+	"tsx",
+	"jsx",
+	"html",
 }
 
 return M
