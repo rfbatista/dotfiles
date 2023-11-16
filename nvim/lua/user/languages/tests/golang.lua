15b6f1d67d81a3d25722dcf88d6e0f57ee2c8d46
rfbatista <renfbatista@gmail.com>
2023-11-16 18:09:35 -0300

    tests config

---
+++ nvim/lua/user/languages/tests/golang.lua
@@ -0,0 +1,74 @@
+local helper = require("user.languages.tests.helper")
+
+local M = {}
+
+local function findTestName()
+	local ts_utils = require("nvim-treesitter.ts_utils")
+	local current_node = ts_utils.get_node_at_cursor()
+	local expr = current_node
+	while expr do
+		if expr:type() == "function_definition" or expr:type() == "function_declaration" then
+			return (ts_utils.get_node_text(expr:child(1)))[1]
+		end
+		expr = expr:parent()
+	end
+	if not expr then
+		return ""
+	end
+	return (ts_utils.get_node_text(expr:child(1)))[1]
+end
+
+local function run_gotest(args, config)
+	local t = {}
+	table.insert(t, "terminal " .. config.go_cmd)
+	if args ~= nil then
+		for _, v in pairs(args) do
+			table.insert(t, v)
+		end
+	end
+	local jest_cmd = table.concat(t, "")
+	vim.api.nvim_command(jest_cmd)
+end
+
+M.run_gotest = run_gotest
+
+M.run = function(args, config)
+	helper.create_window()
+	run_gotest(args, config)
+	helper.focus_last_accessed_window()
+end
+
+M.findTestName = findTestName
+
+M.unit_test = function()
+	local config = {}
+	local c_file = helper.get_current_file_path()
+	local args = {}
+	config.go_cmd = "go test"
+	M.run(args, config)
+end
+
+M.integration_test = function()
+	local config = {}
+	local c_file = helper.get_current_file_path()
+	local args = {}
+	config.go_cmd = "go test"
+	M.run(args, config)
+end
+
+M.benchmark_test = function()
+	local config = {}
+	local c_file = helper.get_current_file_path()
+	local args = {}
+	config.go_cmd = "go test -bench=."
+	local testName = findTestName()
+	local rootPath = helper.find_project_root()
+	local start_idx, end_idx = c_file:find(rootPath)
+	local after = c_file:sub(end_idx + 1)
+	local gotestargs = " ." .. after:match(".*/")
+	table.insert(args, gotestargs)
+	print(gotestargs)
+	M.run(args, config)
+end
+
+return M
