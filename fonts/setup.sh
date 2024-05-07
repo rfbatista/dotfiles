9614fc5b36a965ae2522f19650aec55905c3a883
Renan Batista <renan.batista@gruponewway.com.br>
2024-05-07 17:04:51 -0300

    update

---
+++ fonts/setup.sh
@@ -0,0 +1,4 @@
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
+unzip JetBrainsMono.zip
+mv JetBrainsMonoNerdFont-*.ttf ~/.local/share/fonts/
+sudo mv JetBrainsMonoNerdFont-*.ttf /usr/share/fonts/
--- polybar/setup.sh
+++ polybar/setup.sh
@@ -2,3 +2,4 @@
 chmod +x $HOME/dotfiles/polybar/launch.sh
 ln -s ~/dotfiles/polybar ~/.config/polybar
 chomod +x $HOME/dotfiles/polybar/keyboard-script.sh
+git clone --depth 1 https://github.com/Murzchnvok/polybar-collection
