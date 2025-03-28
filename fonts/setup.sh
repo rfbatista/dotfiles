136dad15f9461d219af53c3c8f6ca70233daaa0b
renan <contato@renandonato.com.br>
2025-03-28 18:21:05 -0300

    work

--- fonts/setup.sh
+++ fonts/setup.sh
@@ -1,4 +1,13 @@
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
-unzip JetBrainsMono.zip
-mv JetBrainsMonoNerdFont-*.ttf ~/.local/share/fonts/
-sudo mv JetBrainsMonoNerdFont-*.ttf /usr/share/fonts/
+unzip JetBrainsMono.zip -d JetBrainsMono
+sudo mv JetBrainsMono /usr/share/fonts/
+fc-cache -fv
+fc-list | grep "JetBrainsMono"
+
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Agave.zip
+unzip Agave.zip -d Agave
+sudo mv Agave /usr/share/fonts/
+fc-cache -fv
+
+
--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
 ln -s $HOME/dotfiles/.profile ~/.profile
+sudo pacman -S unzip
+sudo pacman -S rofi
+sudo pacman -S feh 
+sudo pacman -S zellij
+
